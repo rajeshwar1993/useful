25df3c44a65e8960b19ee208ce1656459d96f3e3
Rajeshwar Rudra <rajeshwar1993@gmail.com>
2020-03-25 12:12:50 +0530

    heap insert done

--- powers/dynamic/Trees/heaps.js
+++ powers/dynamic/Trees/heaps.js
@@ -1,12 +1,46 @@
 class Heap {
-  constructor(r, compareFunction) {
-    this.root = r;
+  constructor(
+    type = "max",
+    compareFunction = (a, b) => {
+      if (type === "max") {
+        return a > b ? 1 : -1;
+      } else return a < b ? 1 : -1;
+    }
+  ) {
+    this.heap = [];
+    this.type = type;
     this.compare = compareFunction;
   }
 
-  insert() {}
+  insert(el) {
+    this.heap.push(el);
 
-  getRoot() {}
+    let currentPos = this.heap.length - 1;
+
+    while (true) {
+      if (currentPos === 0) return currentPos;
+
+      let el = this.heap[currentPos];
+      let parentIndx = Math.floor((currentPos - 1) / 2);
+      let parent = this.heap[parentIndx];
+
+      if (this.compare(parent, el) === 1) return currentPos;
+
+      this.heap[parentIndx] = el;
+      this.heap[currentPos] = parent;
+      currentPos = parentIndx;
+    }
+  }
+
+  extract() {}
+
+  getHeap() {
+    return [...this.heap];
+  }
+
+  getTop() {
+    return this.heap[0];
+  }
 }
 
 export default Heap;
--- test/node.test.js
+++ test/node.test.js
@@ -37,52 +37,127 @@ describe("Dynamic Node Tests", function() {
   });
 
   describe("Tests for Heap", () => {
-    //creating node array for integers
-    let intNodeArray = [];
-    intNodeArray.push(new BTNode(2));
-    intNodeArray.push(new BTNode(12));
-    intNodeArray.push(new BTNode(22));
-    intNodeArray.push(new BTNode(24));
-    intNodeArray.push(new BTNode(5));
-    intNodeArray.push(new BTNode(8));
-    intNodeArray.push(new BTNode(30));
-    intNodeArray.push(new BTNode(17));
-    intNodeArray.push(new BTNode(21));
-    intNodeArray.push(new BTNode(16));
+    //creating array for integers
+    let intInputArray = [];
+    intInputArray.push(29);
+    intInputArray.push(12);
+    intInputArray.push(22);
+    intInputArray.push(24);
+    intInputArray.push(5);
+    intInputArray.push(8);
+    intInputArray.push(30);
+    intInputArray.push(17);
+    intInputArray.push(2);
+    intInputArray.push(16);
 
     // simple heap tests
-    let root1 = new BTNode(10);
-    let compareFunction = () => {};
-    let heap = new Heap(root1, compareFunction);
+    let heap = new Heap();
+    heap.insert(10);
 
     describe("Tests for General heap", () => {
       it("Heap should be a class(function)", () => {
         expect(typeof Heap).to.equal("function");
       });
       it("Heap node data should be integer", () => {
-        expect(typeof heap.root.getData()).to.equal("number");
+        expect(typeof heap.heap[0]).to.equal("number");
       });
       it("Heap node data should be 10", () => {
-        expect(heap.root.getData()).to.equal(10);
+        expect(heap.getTop()).to.equal(10);
       });
     });
 
     // integer Min Heap
-    let root2 = new BTNode(10);
-    let integerMinHeapCompareFunction = () => {};
-    let integerMinHeap = new Heap(root2, integerMinHeapCompareFunction);
+    let integerMinHeap = new Heap("min");
 
     //creating minHeap
-    intNodeArray.forEach(i => {
+    intInputArray.forEach(i => {
       integerMinHeap.insert(i);
     });
 
+    console.log(integerMinHeap.getHeap());
+
     describe("Tests for Integer Min Heap", () => {
       it("Root node data should be number", () => {
-        expect(typeof integerMinHeap.root.getData()).to.equal("number");
+        expect(typeof integerMinHeap.getTop()).to.equal("number");
       });
       it("Root node data should be minimum", () => {
-        expect(integerMinHeap.root.getData()).to.equal(2);
+        expect(integerMinHeap.getTop()).to.equal(2);
+      });
+    });
+
+    // integer Max Heap
+    let integerMaxHeap = new Heap("max");
+
+    //creating maxHeap
+    intInputArray.forEach(i => {
+      integerMaxHeap.insert(i);
+    });
+
+    console.log(integerMaxHeap.getHeap());
+
+    describe("Tests for Integer Max Heap", () => {
+      it("Root node data should be number", () => {
+        expect(typeof integerMaxHeap.getTop()).to.equal("number");
+      });
+      it("Root node data should be Maximum", () => {
+        expect(integerMaxHeap.getTop()).to.equal(30);
+      });
+    });
+
+    //object Array
+
+    //creating object array for integers
+    let objInputArray = [];
+    objInputArray.push({ letter: "X", value: 24 });
+    objInputArray.push({ letter: "D", value: 4 });
+    objInputArray.push({ letter: "A", value: 1 });
+    objInputArray.push({ letter: "C", value: 3 });
+    objInputArray.push({ letter: "G", value: 6 });
+    objInputArray.push({ letter: "K", value: 11 });
+    objInputArray.push({ letter: "O", value: 15 });
+    objInputArray.push({ letter: "Q", value: 17 });
+
+    // object Min Heap
+    let objectMinCompareFunction = (a, b) => {
+      return a["value"] < b["value"] ? 1 : -1;
+    };
+    let objectMinHeap = new Heap("min", objectMinCompareFunction);
+
+    //creating minHeap
+    objInputArray.forEach(i => {
+      objectMinHeap.insert(i);
+    });
+
+    console.log(objectMinHeap.getHeap());
+
+    describe("Tests for object Min Heap", () => {
+      it("Root node data should be number", () => {
+        expect(typeof objectMinHeap.getTop()).to.equal("object");
+      });
+      it("Root node data should be minimum", () => {
+        expect(objectMinHeap.getTop().value).to.equal(1);
+      });
+    });
+
+    // object Max Heap
+    let objectMaxCompareFunction = (a, b) => {
+      return a["value"] > b["value"] ? 1 : -1;
+    };
+    let objectMaxHeap = new Heap("max", objectMaxCompareFunction);
+
+    //creating maxHeap
+    objInputArray.forEach(i => {
+      objectMaxHeap.insert(i);
+    });
+
+    console.log(objectMaxHeap.getHeap());
+
+    describe("Tests for object Max Heap", () => {
+      it("Root node data should be number", () => {
+        expect(typeof objectMaxHeap.getTop()).to.equal("object");
+      });
+      it("Root node data should be Maximum", () => {
+        expect(objectMaxHeap.getTop().value).to.equal(24);
       });
     });
   });
